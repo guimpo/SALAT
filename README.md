9971e52e8ba0d976c6e95ad03af86d59244daac3
andreendo <andreendo@gmail.com>
2018-06-07 15:55:49 -0300

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SALAT
 SALAT - Stupid And Limited Automated Tester
+
+# Enhancements
+
+- Deal with alerts and popups.
+- No action is performed in form elements.
+- Configure to detect faulty pages or states.
+- Add more systematic search strategies.
--- src/test/java/com/github/andreendo/salat/WebTesterTest.java
+++ src/test/java/com/github/andreendo/salat/WebTesterTest.java
@@ -36,10 +36,10 @@ public class WebTesterTest {
     
     @Test
     public void test01() {
-        //Driver driver = new WebAppDriver(webDriver, "http://portal.utfpr.edu.br/", "utfpr.edu.br");       
+        Driver driver = new WebAppDriver(webDriver, "http://portal.utfpr.edu.br/", "utfpr.edu.br");       
         //Driver driver = new WebAppDriver(webDriver, "https://www.wikipedia.org/", "wikipedia.org");   
         //Driver driver = new WebAppDriver(webDriver, "http://localhost:8080", "localhost:8080");       
-        Driver driver = new WebAppDriver(webDriver, "http://demo.redmine.org", "demo.redmine.org");       
+        //Driver driver = new WebAppDriver(webDriver, "http://demo.redmine.org", "demo.redmine.org");       
         
         StopCondition stopCondition = new CounterStopCondition(200);
         Tester tester = new Tester(driver, stopCondition, new Random());
