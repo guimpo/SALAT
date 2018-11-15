696a90fa0c345312182142db25eeb3ca7040a6b3
Paulo Nakaima <nakaima.prk@gmail.com>
2018-11-15 18:24:58 -0200

    Add method getMaxLengthValidation

--- src/main/java/com/github/andreendo/salat/InputElement.java
+++ src/main/java/com/github/andreendo/salat/InputElement.java
@@ -1,5 +1,6 @@
 package com.github.andreendo.salat;
 
+import java.util.AbstractMap.SimpleEntry;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -7,8 +8,11 @@ import org.openqa.selenium.WebElement;
  * @author paulo
  */
 public class InputElement {
+    
+    private static final String TEXT = "text";
+    private static final String NUMBER = "number";
+    
     private WebElement element;
-    private String content;
     
     public void setElement(WebElement element) {
         this.element = element;
@@ -20,10 +24,42 @@ public class InputElement {
 
     @Override
     public String toString() {
-        return "INPUT ELEMENT " + element + " " + content;
+        return "INPUT ELEMENT " + element;
     }
 
-    void setContent(String content) {
-        this.content = content;
+    public SimpleEntry<String, String> getMaxLengthValidation() {
+        String type = element.getAttribute("type");
+        type = type == null ? TEXT : type;
+        
+        String maxKey = "max";
+        String maxValue = null;
+        
+        SimpleEntry<String, String> pair =
+                new SimpleEntry<>(maxKey, maxValue);
+        
+        switch(type) {
+            
+            case TEXT:
+                maxValue = element.getAttribute("maxlength");
+                break;
+                
+            case NUMBER:
+                maxValue = element.getAttribute("max");
+                break;
+                
+            default:
+                return null;
+        }
+        
+        boolean notExistAttributeOrValue = maxValue == null ||
+                maxValue.equals("maxlength") || maxValue.equals("max") ||
+                maxValue.equals("");
+                
+        if(notExistAttributeOrValue)
+            return null;
+            
+        pair.setValue(maxValue);
+        
+        return pair;
     }
 }
